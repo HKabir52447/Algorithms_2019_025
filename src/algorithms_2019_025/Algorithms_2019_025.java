dd8bfc0509aae030c49aaf0e0192136dca1b18ec
MD. Humayun Kabir <humayunkabir52447@gmail.com>
2023-06-12 09:10:33 +0600

    Queue implement

--- src/algorithms_2019_025/Algorithms_2019_025.java
+++ src/algorithms_2019_025/Algorithms_2019_025.java
@@ -96,6 +96,8 @@ public class Algorithms_2019_025 {
                 case 15:
                     break;
                 case 16:
+                    QueueOp q = new QueueOp();
+                    q.queue();
                     break;
                 case 17:
                     break;
